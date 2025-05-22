c22fc4718c6b500df0c47be46ebaca3e55b2b44b
DANIEL JESÚS NOGUERA DURAN <noguera924@gmail.com>
2025-05-23 00:13:43 +0200

    header, se empiza main y primer section

---
+++ app/views/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>StudyLink - Inicio</title>
+  <link rel="stylesheet" href="../../public/css/styles.css">
+</head>
+<body>
+
+  <!-- Encabezado -->
+  <header class="encabezado">
+    <div class="contenedor">
+      <img src="../../public/images/logo_mejorado.png" alt="Logo StudyLink" class="logo">
+      <nav class="navegacion">
+        <a href="#">INICIO</a>
+        <a href="#">¿QUIÉNES SOMOS?</a>
+        <a href="#">SERVICIOS</a>
+        <a href="#">BLOG</a>
+        <a href="#">FAQ</a>
+        <button class="boton-contactanos"><p><strong>CONTÁCTANOS</strong></p></button>
+      </nav>
+    </div>
+  </header>
+
+  <main class="home_main">
+    <section class="main_sec_1">
+        <div>
+            <h1>StudyLink</h1>
+            <button class="butom_ver_tramite"><strong>VER TRAMITES</strong></button>
+        </div>
+        <div></div>
+    </section>
+  </main>
+</body>
+</html>
